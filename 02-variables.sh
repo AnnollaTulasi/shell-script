1e123ac3cf56f1d58f82ac730cfe759304a3da5f
Annolla Tulasi <tulasi.tls.111@gmail.com>
2024-12-30 22:17:59 +0530

    variables added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo "Tulasi : Hi Suma"
-echo "Suma : Hi Tulasi .How are you?"
-echo "Tulasi : I am fine .What about you?"
-echo " Suma: Are you learing any new skills?"
+PERSON1=Tulasi
+PERSON2=Sumathi
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON2 : Hi $PERSON1 .How are you?"
+echo "$PERSON1 : I am fine .What about you?"
+echo "$PERSON2: Are you learing any new skills?"
