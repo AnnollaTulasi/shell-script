a812cf1830ebb613bfa2c7bbbe20ee990800d40c
Annolla Tulasi <tulasi.tls.111@gmail.com>
2024-12-30 22:29:15 +0530

    args as vars

--- 03-varargs.sh
+++ 03-varargs.sh
@@ -4,5 +4,5 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1 : Hi  $PERSON2"
-echo"$PERSON2 : Hello .How are you ?"
+echo "$PERSON2 : Hello .How are you ?"
 echo "$PERSON1 : Please learn and earn,you should be only earning and grow your carrer and yourself on daily bases"
