9e58b7784c32c27711a8fec69187ca98f6deff8c
Annolla Tulasi <tulasi.tls.111@gmail.com>
2024-12-30 21:58:24 +0530

    added some msg

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,4 +1,5 @@
 #!bin/bash
 #Shebang-the first line is not commented and to validate the commands we enter we should add #!/bin/bash
 
-echo "hello World!"
+echo "H1ello World!"
+echo "Hi Tulasi ,have you started the shell scripting?"
