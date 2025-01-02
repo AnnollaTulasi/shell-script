5a7ca4a15779056eeaee292a54c0578da5810565
Annolla Tulasi <tulasi.tls.111@gmail.com>
2025-01-02 21:55:32 +0530

    Functions

---
+++ 12-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .... $R FAILURE"
+        exit 1
+    else
+        echo - e "$2 .... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR :: You must have sudo access to install the packages"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "Installing Mysql"
+else
+    echo -e "Already mysql is  $Y installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo -e "Already Git is  $Y installed"
+fi
