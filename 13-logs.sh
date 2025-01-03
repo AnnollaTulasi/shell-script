93074dc3e76abada93d54e5832f73c5102287afa
Annolla Tulasi <tulasi.tls.111@gmail.com>
2025-01-03 09:44:54 +0530

    Logs file

---
+++ 13-logs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+LOGS_FOLDER=/var/log/script-logs
+LOG_FILE=$(echo $0 | cut -d "." -f1)   #gets file name and removes the  .sh part
+TIMESTAMP=$(date + %Y-%m-%d_%H:%M:%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE_NAME-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .... $R FAILURE"
+        exit 1
+    else
+        echo - e "$2 .... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR :: You must have sudo access to install the packages"
+    exit 1
+fi
+
+dnf list installed mysql &>>$LOG_FILE
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "Installing Mysql"
+else
+    echo -e "Already mysql is  $Y installed"
+fi
+
+dnf list installed git &>>$LOG_FILE
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE
+    VALIDATE $? "Installing Git"
+else
+    echo -e "Already Git is  $Y installed"
+fi
