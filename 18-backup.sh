dc325ff3c4115eee6c9cd9c6c44cf09568969d61
Annolla Tulasi <tulasi.tls.111@gmail.com>
2025-01-06 20:29:12 +0530

    wordcount

---
+++ 18-backup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER=/var/log/script-logs
+LOG_FILE=$(echo $0 | cut -d "." -f1)   #gets file name and removes the  .sh part
+TIMESTAMP=$(date +%Y-%m-%d-%H:%M:%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+mkdir -p /var/log/script-logs
+
+USAGE(){
+    echo -e "$R USAGE :: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
+    exit 1
+}
+
+if[ $# -ne 2 ]
+then 
+    USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "Souce dir $1 ..... is not present"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+    echo  -e "Dest Dir $2 ..... is not present"
+    exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+
+if [ -n $FILES ]
+then
+    echo "Files are : $FILES"
+else
+    echo "No files to zip"
+fi
