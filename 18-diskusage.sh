ff4484981f8dab8559d233b1129b8969373d8975
Annolla Tulasi <tulasi.tls.111@gmail.com>
2025-01-07 22:21:14 +0530

    backup

--- 18-diskusage.sh
+++ 18-diskusage.sh
@@ -8,4 +8,10 @@ do
     USAGE=$(echo $line | awk -F " " '{print $6F}' |cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     echo "PARTITION: $PARTITION ,USAGE: $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk Usage on partition : $PARTITION USAGE is $USAGE \n"
+    fi
 done <<< $DISK_USAGE
+
+echo -e "MSG $MSG"
