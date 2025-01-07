e56a733b972547b656666ee8a9a3b8924013bcbf
Annolla Tulasi <tulasi.tls.111@gmail.com>
2025-01-07 22:12:37 +0530

    backup

--- 18-diskusage.sh
+++ 18-diskusage.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
 
 while read -r line
 do
-    echo $line
+    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "PARTITION: $PARTITION ,USAGE: $USAGE"
 done <<< $DISK_USAGE
